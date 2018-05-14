96f582393f6d0e00504503cf3d0681409be55fe9
Max Pereira <maaperei@ucsc.edu>
2018-05-13 18:18:05 -0700

    Made sample story load into textarea automatically for debug

--- seshat.js
+++ seshat.js
@@ -4,6 +4,13 @@ seshat.js | Main Game Engine Script
 Max Pereira
 */
 
+// Debug function to set the textarea contents to our demo story
+window.onload = function() {
+    // storytext is set in debug.txt to be the contents of sample-adventure.json
+    document.getElementById("advTA").value = storytext;
+};
+
+// Load the story in the textarea into our engine
 function loadStory() {
     storyRawJSON = document.getElementById("advTA").value;
     s = JSON.parse(storyRawJSON);
